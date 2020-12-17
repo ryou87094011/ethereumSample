0b338ba3f29743e250c8602a4757c4f9e274f52c
ryou87094011 <uehara39625@icloud.com>
2020-12-17 18:28:44 +0900

    ログイン画面作成

---
+++ src/components/Login.vue
@@ -0,0 +1,37 @@
+<template>
+<div class="mx-auto">
+  <div class="card mx-auto" style="width: 30rem;">
+      <img src="../../img/logo.png" class="rounded mx-auto d-block" alt="logo">
+      <div class="card-body">
+            <form>
+      <div class="form-group row">
+        <label for="staticEmail" class="col-2 col-form-label"><b>ID</b></label>
+        <input type="text" class="col-8 form-control" id="inputEmail" placeholder="hogehoge@example.com" :value="email">
+      </div>
+      <div class="form-group row" style="margin-top: 0.5rem;">
+        <label for="inputPassword" class="col-2 col-form-label"><b>PW</b></label>
+        <input type="password" class="col-8 form-control" id="inputPassword" :value="password">
+      </div>
+    </form>
+    <button type="button" class="btn btn-primary" style="margin-top: 2rem;">ログイン</button>
+      </div>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Main',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      email: '',
+      password: ''
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Main from '@/components/Main'
+import Login from '@/components/Login'
 
 Vue.use(Router)
 
@@ -8,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Main',
-      component: Main
+      name: 'Login',
+      component: Login
     }
   ]
 })
